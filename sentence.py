77e339890bc3b904260e05b52ccbc14ae2ee3ee2
DNYANESHWAR JAGANNATH PATIL <89774901+dnyaneshwar2408@users.noreply.github.com>
2023-06-04 10:01:42 +0530

    Add files via upload

---
+++ sentence.py
@@ -0,0 +1,47 @@
+import re
+alphabets= "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr|www|Prof|http|https)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|Prof|Capt|Cpt|Lt|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov|edu|me)"
+digits = "([0-9])"
+multiple_dots = r'\.{2,}'
+
+def split_into_sentences(text: str) -> list[str]:
+   
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2",text)
+    text = re.sub(multiple_dots, lambda match: "<prd>" * len(match.group(0)) + "<stop>", text)
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = [s.strip() for s in sentences]
+    if sentences and not sentences[-1]: sentences = sentences[:-1]
+    return sentences
+paragraph = "Mr. Johnson visited www.example.com. He bought 4.5x6.7 inches of fabric. Mrs. Smith, Dr. Brown, and Prof. Davis attended the meeting. The company, XYZ Inc., announced its merger with ABC Ltd. Jr. is excited about the upcoming event. They went to the U.S.A. for vacation. Another sentence follows."
+sentences = split_into_sentences(paragraph)
+for sentence in sentences:
+    print(sentence)
+print("-------------------------------------------[]")
+paragraph = "I live in the U.K. and work for XYZ Corp. The meeting is scheduled for 2 p.m. on June 5th. Please bring your ID card. The cost is $5.99. John Jr. and his friend Mr. Smith went to the U.S.A. last week. They visited www.example.org for more information. Another sentence follows."
+sentences = split_into_sentences(paragraph)
+for sentence in sentences:
+    print(sentence)
